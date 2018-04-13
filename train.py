53320c43b6cfe06f0af36e826dcdfcaf8e63e03e
Jay Harris <jay.harris@outlook.co.nz>
2018-04-13 22:00:00 +1000

    WIP converting example to using torch

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import torch
-import numpy as np
+from torch.autograd import Variable
 
+dtype = torch.FloatTensor
 
 class Data:
     def __init__(self, filename):
@@ -42,12 +43,12 @@ hidden_size = 100
 seq_length = 200
 learning_rate = 1e-1
 
-# model parameters
-Wxh = np.random.randn(hidden_size, vocab_size)*0.01  # input to hidden
-Whh = np.random.randn(hidden_size, hidden_size)*0.01  # hidden to hidden
-Why = np.random.randn(vocab_size, hidden_size)*0.01  # hidden to output
-bh = np.zeros((hidden_size, 1))  # hidden bias
-by = np.zeros((vocab_size, 1))  # output bias
+input_weights = Variable(torch.randn(hidden_size, vocab_size).type(dtype) * 0.01, require_grad=True)
+hidden_weights = Variable(torch.randn(hidden_size, hidden_size).type(dtype) * 0.01, require_grad=True)
+output_weights = Variable(torch.randn(hidden_size, hidden_size).type(dtype) * 0.01, require_grad=True)
+
+hidden_biases = Variable(torch.zeros(hidden_size, 1), require_grad=False)
+output_biases = Variable(torch.zeros(vocab_size, 1), require_grad=False)
 
 
 def lossFun(inputs, targets, hprev):
@@ -56,41 +57,29 @@ def lossFun(inputs, targets, hprev):
     hprev is Hx1 array of initial hidden state
     returns the loss, gradients on model parameters, and last hidden state
     """
-    xs, hs, ys, ps = {}, {}, {}, {}
-    hs[-1] = np.copy(hprev)
+    input_at_t, hidden_at_t, output_at_t, ps = {}, {}, {}, {}
     loss = 0
-    # forward pass
+
+    # for each character in the input
     for t in range(len(inputs)):
-        xs[t] = np.zeros((vocab_size, 1))  # encode in 1-of-k representation
-        xs[t][inputs[t]] = 1
-        hs[t] = np.tanh(np.dot(Wxh, xs[t]) +
-                        np.dot(Whh, hs[t-1]) + bh)  # hidden state
+        input_at_t[t] = torch.zeros(vocab_size, 1)  # encode in 1-of-k representation
+        input_at_t[t][inputs[t]] = 1 # set the current character as true
+
+        x = input_at_t[t]
+
+        y_pred = x.mm(input_weights).clamp(min=0).mm(w2)
+        
+        hs[t] = (input_weights.mm(xs[t]) + hidden_weights.mm(hs[t - 1]) + hidden_biases).tanh()
+
         # unnormalized log probabilities for next chars
-        ys[t] = np.dot(Why, hs[t]) + by
+        ys[t] = hidden_weights.mm(hs[t]) + output_biases
+
         # probabilities for next chars
-        ps[t] = np.exp(ys[t]) / np.sum(np.exp(ys[t]))
+        ps[t] = (ys[t] / ys[t].exp().sum()).exp()
+        
         loss += -np.log(ps[t][targets[t], 0])  # softmax (cross-entropy loss)
     # backward pass: compute gradients going backwards
-    dWxh, dWhh, dWhy = np.zeros_like(
-        Wxh), np.zeros_like(Whh), np.zeros_like(Why)
-    dbh, dby = np.zeros_like(bh), np.zeros_like(by)
-    dhnext = np.zeros_like(hs[0])
-    for t in reversed(range(len(inputs))):
-        dy = np.copy(ps[t])
-        # backprop into y. see http://cs231n.github.io/neural-networks-case-study/#grad if confused here
-        dy[targets[t]] -= 1
-        dWhy += np.dot(dy, hs[t].T)
-        dby += dy
-        dh = np.dot(Why.T, dy) + dhnext  # backprop into h
-        dhraw = (1 - hs[t] * hs[t]) * dh  # backprop through tanh nonlinearity
-        dbh += dhraw
-        dWxh += np.dot(dhraw, xs[t].T)
-        dWhh += np.dot(dhraw, hs[t-1].T)
-        dhnext = np.dot(Whh.T, dhraw)
-    for dparam in [dWxh, dWhh, dWhy, dbh, dby]:
-        # clip to mitigate exploding gradients
-        np.clip(dparam, -5, 5, out=dparam)
-    return loss, dWxh, dWhh, dWhy, dbh, dby, hs[len(inputs)-1]
+    
 
 
 def sample(h, seed_ix, n):
